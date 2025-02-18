e15a7e2f2218220d6499d7c681f6381b4f8fc67d
Zeey76 <abdulrasaqzainab76@gmail.com>
2025-02-18 23:35:51 +0100

    .

--- my-next-app/src/app/(main)/products/(product)/category/[category]/page.tsx
+++ my-next-app/src/app/(main)/products/(product)/category/[category]/page.tsx
@@ -6,14 +6,15 @@ const CategoryPage = async ({params}: {
 })=> {
   const { category } = await params;
   const products = await getProductsByCategory(category);
-  console.log(products)
   return (
     <>
       <h2 className="bg-black w-full p-8 md:12 text-white uppercase text-center font-bold">{category}</h2>
       <div className="flex flex-col gap-8 w-full mt-8">
-        {/* {products.map((product) => {
+        {products.map((product) => (
+          <div key={product.id} className="flex flex-col gap-4">
             
-        })} */}
+          </div>
+        ))}
       </div>
     </>
   );
---
+++ my-next-app/src/app/(main)/products/(product)/slug/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      jhkkjk
+    </div>
+  )
+}
+
+export default page
